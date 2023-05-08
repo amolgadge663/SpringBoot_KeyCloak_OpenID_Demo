22de70aca08e95b0e5fa7ad0aad3d184638c9327
Amol Kailas Gadage <amol.gadage@incedoinc.com>
2023-05-08 19:12:02 +0530

    Initial Commit

---
+++ src/main/java/com/amolsoftwares/www/springbootkeycloakdemo/SpringBootKeyCloakDemoApplication.java
@@ -0,0 +1,13 @@
+package com.amolsoftwares.www.springbootkeycloakdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootKeyCloakDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootKeyCloakDemoApplication.class, args);
+    }
+
+}
